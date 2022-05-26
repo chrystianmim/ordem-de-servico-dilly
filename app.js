9791bd48432a430d978f560ae9f90f5e670c7b50
chrystianmim <chrystianmim@gmail.com>
2022-05-26 00:01:09 -0300

    post do form OK, criado .gitignore

--- app.js
+++ app.js
@@ -3,14 +3,21 @@ const app = express();
 
 /* Rotas */
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 
-app.get("/index.html", function(req, res) {
-  res.sendFile(__dirname + "/index.html");
+app.get("/home", function(req, res) {
+  res.sendFile(__dirname + "/public/index.html");
 });
 
-app.get("/pages/cadastro-geral/cadastroSetor.html", function(req, res) {
-  res.sendFile(__dirname + "/pages/cadastro-geral/cadastroSetor.html");
+app.get("/pagCadastroSetor", function(req, res) {
+  res.sendFile(__dirname + "/public/pages/cadastro-geral/cadastroSetor.html");
 });
+
+app.post("/pagCadastroSetor", function(req, res) {
+  var setor = req.body.setor;
+  res.sendFile(__dirname + "/public/pages/cadastro-geral/cadastroSetor.html");
+})
 
 app.listen(3000);
