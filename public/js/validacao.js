dd13663d85801b55a3cf1da48586807065a551ce
chrystianmim <chrystianmim@gmail.com>
2022-05-26 19:43:56 -0300

    alterado layout do formulário de função e início da validação deste

--- public/js/validacao.js
+++ public/js/validacao.js
@@ -1,4 +1,4 @@
-// validação para formulário de cadastro de setores;
+// validação para formulário de cadastro de funções;
 
 $('#formSetor').submit(function() { // função submit do jQuery direto no form
   let setor = document.querySelector('#setor');
@@ -7,7 +7,25 @@ $('#formSetor').submit(function() { // função submit do jQuery direto no form
   // inicia a função tornando o erro invisível
   erro.classList.add('d-none');  
 
-  if (setor.value == '') { // mostra erro se o valor do setor estiver vazio
+  if (setor.value == '') { // mostra erro se o valor da função estiver vazio
+    erro.classList.remove('d-none');
+    setor.focus();
+    return false
+  }
+
+  funcao.value = setor.value.toUpperCase(); // transforma a função em uppercase
+  return true
+});
+
+$('#formFuncao').submit(function () { // função submit do jQuery direto no form
+  let funcao = document.querySelector('#funcao');
+  let descricaoFuncao = document.querySelector('#descricaoFuncao');
+  let erro = document.querySelector('#erro');
+
+  // inicia a função tornando o erro invisível
+  erro.classList.add('d-none');
+
+  if (funcao.value == '') { // mostra erro se o valor do setor estiver vazio
     erro.classList.remove('d-none');
     setor.focus();
     return false
