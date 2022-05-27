f56c3b663bf8dd7f47167a6f026d0e0b47b70f42
chrystianmim <chrystianmim@gmail.com>
2022-05-26 22:52:57 -0300

    criado o arquivo 'rascunhos' adicionado algumas notas

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const mysql = require("mysql");
 
 /* Rotas */
 
-app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 app.use(express.static(__dirname + "/public"));
 
 app.get("/home", function(req, res) {
@@ -17,20 +17,22 @@ app.get("/pagCadastroSetor", function(req, res) {
   res.sendFile(__dirname + "/public/pages/cadastro-geral/cadastroSetor.html");
 });
 
+app.get("/pagCadastroFuncao", function (req, res) {
+  res.sendFile(__dirname + "/public/pages/cadastro-geral/cadastroFuncao.html");
+});
+
+
+/* Posts */
+
 app.post("/pagCadastroSetor", function(req, res) {
   var setor = req.body.setor;
   res.sendFile(__dirname + "/public/pages/cadastro-geral/cadastroSetor.html");
 })
 
-app.get("/pagCadastroFuncao", function (req, res) {
-  res.sendFile(__dirname + "/public/pages/cadastro-geral/cadastroFuncao.html");
-});
-
 app.post("/pagCadastroFuncao", function (req, res) {
   var funcao = req.body.funcao;
+  // INCLUIR VAR CBO
   var descricaoFuncao = req.body.descricaoFuncao;
-  console.log(funcao);
-  console.log(descricaoFuncao);
   res.sendFile(__dirname + "/public/pages/cadastro-geral/cadastroFuncao.html");
 })
 
---
+++ rascunho.md
@@ -0,0 +1,20 @@
+cadastro setor
+	- setor
+
+cadastro função
+	- nome função
+	- cbo
+
+cadastro OS
+	- nome função (auto)
+	- cbo (auto)
+  - setor (dropdown)
+	- descrição
+	- EPIs recomendados
+    - Obrigatórios
+		- Quando aplicáveis
+
+emitir OS
+  - código
+  - nome
+  - 
