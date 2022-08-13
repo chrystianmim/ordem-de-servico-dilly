af982246aaf832ffc9914c0630b8e18abe8dad8f
chrystianmim <chrystianmim@gmail.com>
2022-08-12 21:48:29 -0300

    criando html base para pdf

--- pdf.js
+++ pdf.js
@@ -3,7 +3,7 @@ const pdf = require("html-pdf");
 
 // pdf parameters
 const options = { format: 'a4' };
-var html = `this is my pdf you fucker`;
+var html = `TA MALUCO MEUAHMIGO`;
 
 /* async function createPdf () {
   await pdf.create(html, options).toFile("./tmp/OrdemDeServico.pdf", (err, res) => {
@@ -14,9 +14,18 @@ var html = `this is my pdf you fucker`;
 
 module.exports = createPdf; */
 
-
-module.exports = pdf.create(html, options).toFile("./tmp/OrdemDeServico.pdf", (err, res) => {
+var result = pdf.create(html, options).toFile("./tmp/OrdemDeServico.pdf", (err, res) => {
   if (err) throw err;
   console.log('criei pdf');
 });
+
+console.log(result)
+
+module.exports = result;
+
+
+/* module.exports = pdf.create(html, options).toFile("./tmp/OrdemDeServico.pdf", (err, res) => {
+  if (err) throw err;
+  console.log('criei pdf');
+}); */
 
